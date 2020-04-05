5e5182304ea7fd5fa3fc40be588d7f3629b8352c
Rickard Andersson <dunklas@gmail.com>
2020-04-05 20:18:48 +0200

    Add Chip8 struct and init it with some values

---
+++ src/chip8.rs
@@ -0,0 +1,33 @@
+pub fn new() -> Chip8 {
+    Chip8 {
+        op_code: 0,
+        memory: [0; 4096],
+        v: [0; 16],
+        index: 0,
+        program_counter: 0,
+        gfx: [0; 62 * 32],
+        delay_timer: 0,
+        sound_timer: 0,
+        stack: [0; 16],
+        stack_pointer: 0,
+        key: [0; 16]
+    }
+}
+
+pub struct Chip8 {
+    op_code: u16,
+    memory: [u8; 4096],
+    v: [u8; 16],
+    index: u16,
+    program_counter: u16,
+    gfx: [u8; 62 * 32],
+    delay_timer: u8,
+    sound_timer: u8,
+    stack: [u16; 16],
+    stack_pointer: u16,
+    key: [u8; 16]
+}
+
+impl Chip8 {
+    
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod chip8;
+
+fn main() {
+    let chip8 = chip8::new();
+    println!("Hello, world!");
+}
